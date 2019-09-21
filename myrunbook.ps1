fe08924bf190bd9715206b0eabc443ce8d5c0ef4
josephazzopardi <joseph.azzopardi@live.com>
2019-09-21 11:02:49 +0200

    added parameters

--- myrunbook.ps1
+++ myrunbook.ps1
@@ -1,5 +1,24 @@
 #auto test dummy powershell script
 
-Write-Host "This is an Azure Automation Test"
-Get-CimInstance -ClassName Win32_ComputerSystem
+Write-Host "*** This is an Azure Automation Test ***"
+Write-Host "****************************************"
+
+param
+(
+    [bool]$network = $false,
+[bool]$info = $false
+)
+
+
+if($network) {
+   Get-NetIPAddress | Format-Table
+}else {
+   Write-Output ("Network output disabled. Please add parameter value.")
+}
+
+if($info) {
+   Get-CimInstance -ClassName Win32_ComputerSystem
+}else {
+   Write-Output ("Info output disabled. Please add parameter value.")
+}
 
